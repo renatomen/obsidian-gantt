b58744c00a9759529c08ed2a4e54da55272a5ea7
Renato Mendonca <renatomen@gmail.com>
2025-09-25 08:46:37 +0000

    OG-7: Complete Development Infrastructure Setup
    Merge infrastructure setup with Svelte 5 + SVAR Gantt integration

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { Plugin } from 'obsidian';
+
+export default class ObsidianGanttPlugin extends Plugin {
+  async onload() {
+    console.log('Loading Obsidian Gantt plugin');
+    
+    // TODO: Initialize plugin components
+    // This is a placeholder implementation for the development infrastructure setup
+  }
+
+  onunload() {
+    console.log('Unloading Obsidian Gantt plugin');
+  }
+}
