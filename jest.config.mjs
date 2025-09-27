1fe1ccb7e04745f06911f504955a2076e17153dc
renatomen <renatomen@gmail.com>
2025-09-27 15:51:01 +1200

    refactor(orchestration): add DI-enabled SyncOrchestrator and align Jest config for ESM [OG-50]

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,18 +1,33 @@
 /** @type {import('jest').Config} */
 const config = {
   testEnvironment: 'node',
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest', {
       jsc: {
         parser: { syntax: 'typescript', tsx: true },
         target: 'es2020'
       },
-      module: { type: 'commonjs' }
+      module: { type: 'es6' }
+    }],
+    '^.+\\.mjs$': ['@swc/jest', {
+      jsc: {
+        parser: { syntax: 'ecmascript' },
+        target: 'es2020'
+      },
+      module: { type: 'es6' }
     }]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'mjs'],
-  roots: ['<rootDir>/src', '<rootDir>/test'],
-  testMatch: ['**/*.test.ts']
+  roots: ['<rootDir>/src', '<rootDir>/test', '<rootDir>/scripts'],
+  testMatch: ['**/*.test.ts'],
+  moduleNameMapping: {
+    '^(\\.{1,2}/.*)\\.mjs$': '$1'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@cucumber/gherkin)/)'
+  ]
 };
 
 export default config;
